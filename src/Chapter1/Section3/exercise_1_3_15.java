103d4ce1e1cc7f54f4c5f620aa4725d9142e5cf7
Nade Kang <kangnade@gmail.com>
2018-05-25 16:16:36 +0800

    Added exercise 1.3.15 answer

---
+++ src/Chapter1/Section3/exercise_1_3_15.java
@@ -0,0 +1,28 @@
+package Chapter1.Section3;
+
+import edu.princeton.cs.algs4.*;
+
+public class exercise_1_3_15 {
+    // Print method that prints the k-th item(here is a String)
+    public static void printStr(Queue<String> s, int k){
+        int count = 0;
+        int point = s.size() - k;
+
+        while(true){
+            if(point == count) {
+                StdOut.print(s.dequeue());
+                break;
+            }
+            s.dequeue(); // continue to dequeue and add count
+            count++;
+        }
+    }
+    public static void main(String[] args){
+        int k = Integer.parseInt(args[0]);
+        Queue<String> s = new Queue<String>();
+        while(!StdIn.isEmpty()){
+            s.enqueue(StdIn.readString());
+        }
+        printStr(s,k);
+    }
+}
